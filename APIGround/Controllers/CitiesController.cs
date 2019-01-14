edbdf4ef8918924fc5a559d7b70260ef4ab194aa
shahriar hossain <shahriar_cse@live.com>
2019-01-14 19:57:42 +0600

    AddMVC service injected

---
+++ APIGround/Controllers/CitiesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace APIGround.Controllers
+{
+    [Route("api/[controller]")]
+    public class CitiesController : Controller
+    {
+        [Route("GetCities")]
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<Object>()
+            {
+                new {id= 1, name= "Dhaka"},
+                new {id = 2, name = "Rajshahi"}
+            });
+        }
+    }
+}
